8df28215db5fddf2d74b5849518370874ea4b3ea
JoaoVitorHernandes <149633314+JoaoVitorHernandes@users.noreply.github.com>
2024-04-03 11:08:07 -0300

    Add files via upload

--- CPU.java
+++ CPU.java
@@ -1,4 +1,29 @@
-package PBL5;
+public class CPU{
+    private String tipo;
+    private int velocidade;
+    private int threads;
+    private int tdp;
+    private int codCpu;
+    private String gpus;
+    private int nucleos;
+    private float frequencia;
+    private String soquete;
 
-public class CPU extends Produto {
-}
+
+
+    public CPU(String tipo, int velocidade, int threads, int tdp, int codCpu,
+               String gpus, int nucleos, float frequencia,String soquete) {
+        this.tipo = tipo;
+        this.velocidade = velocidade;
+        this.threads = threads;
+        this.tdp = tdp;
+        this.codCpu = codCpu;
+        this.gpus = gpus;
+        this.nucleos = nucleos;
+        this.frequencia = frequencia;
+        this.soquete = soquete;
+
+        super(String descricao, float preco, String marca, String modelo)
+    }
+};
+
