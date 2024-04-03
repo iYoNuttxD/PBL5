13595f48d51f6a58315d690fd14149b375c4e871
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-04-03 11:06:54 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -1,4 +1,16 @@
 package PBL5;
 
 public class Produto {
+    protected int cod_Produto;
+    protected String marca;
+    protected String modelo;
+    protected String descricao;
+    protected float preco;
+
+    public Produto(String marca, String modelo, String descricao, float preco) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
 }
