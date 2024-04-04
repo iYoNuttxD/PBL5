6df4133f21d26e3d7cdb60420a62aa9059a02557
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-04-04 10:19:23 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -13,4 +13,44 @@ public class Produto {
         this.descricao = descricao;
         this.preco = preco;
     }
+    
+    public int getCod_Produto(){
+        return cod_Produto;
+    }
+    
+    public String getMarca(){
+        return marca;
+    }
+    
+    public String getModelo(){
+        return modelo;
+    }
+    
+    public String getDescricao(){
+        return descricao;
+    }
+    
+    public float getPreco(){
+        return preco;
+    }
+    
+    public void setCod_Produto(int cod_Produto){
+        this.cod_Produto = cod_Produto;
+    }
+    
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+    
+    public void setPreco(float preco){
+        this.preco = preco;
+    }
 }
