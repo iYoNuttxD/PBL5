939e689f7272ff12e475754402f0ef7645e07f4a
Pedro Cabral <126737207+phsilvacabral@users.noreply.github.com>
2024-04-06 16:35:43 -0300

    Update Main.java descadastrar produto

--- Main.java
+++ Main.java
@@ -3,6 +3,28 @@ package PBL5;
 import java.util.Scanner;
 
 public class Main {
+
+    public static void descadastrar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira o código do produto que deseja descadastrar:");
+        String codigo = scanner.nextLine();
+
+        Produto produtoParaRemover = null;
+        for (Produto produto : produtos) {
+            if (produto.getCodigo().equals(codigo)) {
+                produtoParaRemover = produto;
+                break;
+            }
+        }
+
+        if (produtoParaRemover != null) {
+            produtos.remove(produtoParaRemover);
+            System.out.println("Produto descadastrado com sucesso!");
+        } else {
+            System.out.println("Produto não encontrado.");
+        }
+    }
+    
     public static void main(String [] args){
         System.out.println("Equipe: Daniel Ganz Musse, João Vitor de Souza Hernandes, " +
                 "Pedro Henrique Silva Cabral, Fabio Augusto Gortz");
@@ -20,7 +42,8 @@ public class Main {
                 if (opcao.equals("1")) {
                     System.out.println("Implementar cadastrar produto");
                 } else if (opcao.equals("2")){
-                    System.out.println("Implementar descadastrar produto");
+                    System.out.println("Descadastrar produto");
+                    descadastrar();
                 } else if (opcao.equals("3")) {
                     System.out.println("Implementar visualizar estoque");
                 } else if (opcao.equals("0")){
