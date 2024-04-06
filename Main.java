6ec10ae1ba5ba5cb89864566204b73270ae2f535
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-04-06 17:51:31 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,37 @@ package PBL5;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String [] args){
+        System.out.println("Equipe: Daniel Ganz Musse, João Vitor de Souza Hernandes, " +
+                "Pedro Henrique Silva Cabral, Fabio Augusto Gortz");
+
+        while (true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("---------- Menu Inicial ----------");
+                System.out.println("O que você deseja fazer?");
+                System.out.println("1. Cadastrar Produto");
+                System.out.println("2. Descadastrar Produto");
+                System.out.println("3. Visualizar Estoque");
+                System.out.println("0. Sair");
+                int opcao = scanner.nextInt();
+                if (opcao == 1) {
+                    System.out.println("Implementar cadastrar produto");
+                } else if (opcao == 2){
+                    System.out.println("Implementar descadastrar produto");
+                } else if (opcao == 3) {
+                    visualizarEstoque();
+                } else if (opcao == 0){
+                    System.out.println("Obrigado por usar o sistema!!!");
+                    break;
+                } else {
+                    System.out.println("Opção inválida!!!");
+                }
+            } catch (Exception e) {
+                System.out.println("Ocorreu um erro: " + e.getMessage());
+            }
+        }
+    }
 
     public static void descadastrar() {
         Scanner scanner = new Scanner(System.in);
@@ -25,29 +56,27 @@ public class Main {
         }
     }
     
-    public static void main(String [] args){
-        System.out.println("Equipe: Daniel Ganz Musse, João Vitor de Souza Hernandes, " +
-                "Pedro Henrique Silva Cabral, Fabio Augusto Gortz");
-
-        while (true) {
+    public static void visualizarEstoque() {
+        while (true){
             try {
                 Scanner scanner = new Scanner(System.in);
-                System.out.println("---------- BEM VINDO ----------");
-                System.out.println("O que você deseja fazer?");
-                System.out.println("1. Cadastrar Produto");
-                System.out.println("2. Descadastrar Produto");
-                System.out.println("3. Visualizar Estoque");
+                System.out.println("---------- Visualizar Estoque ----------");
+                System.out.println("Qual estoque deseja visualizar?");
+                System.out.println("1. CPU");
+                System.out.println("2. GPU");
+                System.out.println("3. Placa Mãe");
+                System.out.println("4. Memóriam RAM");
                 System.out.println("0. Sair");
-                String opcao = scanner.nextLine();
-                if (opcao.equals("1")) {
-                    System.out.println("Implementar cadastrar produto");
-                } else if (opcao.equals("2")){
-                    System.out.println("Descadastrar produto");
-                    descadastrar();
-                } else if (opcao.equals("3")) {
-                    System.out.println("Implementar visualizar estoque");
-                } else if (opcao.equals("0")){
-                    System.out.println("Obrigado por usar o sistema!!!");
+                int opcao = scanner.nextInt();
+                if (opcao == 1) {
+                    CPU.visualizarEstoqueCPU();
+                } else if (opcao == 2){
+                    GPU.visualizarEstoqueGPU();
+                } else if (opcao == 3) {
+                    Placa_Mae.visualizarEstoquePlacaMae();
+                } else if (opcao == 4){
+                    RAM.visualizarEstoqueRAM();
+                } else if (opcao == 0){
                     break;
                 } else {
                     System.out.println("Opção inválida!!!");
@@ -56,6 +85,5 @@ public class Main {
                 System.out.println("Ocorreu um erro: " + e.getMessage());
             }
         }
-
     }
 }
