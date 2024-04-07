836a9ce5078b453a96c156762ffa1dbf172cfe00
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-04-07 04:36:15 -0300

    Update GPU.java

--- GPU.java
+++ GPU.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class GPU extends Produto {
     private String pcie;
     private int nucleos;
@@ -8,6 +10,7 @@ public class GPU extends Produto {
     private int slot;
     private int tamanho;
     private String conector;
+    private static ArrayList<GPU> gpus = new ArrayList<GPU>();
 
     public GPU(String marca, String modelo, String descricao, float preco, String pcie, int nucleos,
                int tam_Mem, int vel_Mem, String tipo_Mem, int tdp, int slot, int tamanho, String conector,
@@ -22,26 +25,55 @@ public class GPU extends Produto {
         this.slot = slot;
         this.tamanho = tamanho;
         this.conector = conector;
+
+    }
+
+    public static void visualizarEstoqueGPU() {
+        int i = 1;
+        for (GPU gpuItem : gpus) {
+            System.out.println(i + "// " +  gpuItem);
+            i++;
+        }
+    }
+
+    public static boolean verificarCod_ProdutoGPU(int Cod_Produto) {
+        for (GPU gpu : gpus) {
+            if (gpu.getCod_Produto() == Cod_Produto) {
+                System.out.println(gpu);
+                return true; // O código de produto já está cadastrado
+            }
+        }
+        return false; // O código de produto não está cadastrado
+    }
+
+    public void adicionarEstoqueGPU(GPU gpu){
+        gpus.add(gpu);
+    }
+
+    public static void removerGPU(int cod_Produto) {
+        for (int i = 0; i < gpus.size(); i++) {
+            GPU gpu = gpus.get(i);
+            if (gpu.getCod_Produto() == cod_Produto) {
+                gpus.remove(i); // Remove a CPU da lista
+                break;
+            }
+        }
     }
 
 
-@Override
-public String toString() {
-    return getCod_Produto() + " - " + getDescricao() + " (" +
-            "Modelo = '" + getModelo() + '\'' +
-            ", Marca = '" + getMarca() + '\'' +
-            ", Preço = " + getPreco() +
-            ", PCIe = '" + pcie + '\'' +
-            ", Núcleos = " + nucleos +
-            ", Tamanho da Memória = " + tam_Mem + "GB" +
-            ", Velocidade da Memória = " + vel_Mem + "MHz" +
-            ", Tipo de Memória = '" + tipo_Mem + '\'' +
-            ", TDP = " + tdp + "W" +
-            ", Slot = " + slot +
-            ", Tamanho = " + tamanho + "mm" +
-            ", Conector = '" + conector + '\'' +
-            ')';
-}
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", Núcleos: " + nucleos +
+                ", Tamanho da Memória: " + tam_Mem +
+                ", Velocidade da Memória: " + vel_Mem +
+                ", Tipo de Memória: " + tipo_Mem +
+                ", TDP: " + tdp +
+                ", Conector: " + conector +
+                ", PCIe: " + pcie +
+                ", Slot: " + slot +
+                ", Tamanho: " + tamanho;
+    }
 
     public String getPcie(){
         return pcie;
