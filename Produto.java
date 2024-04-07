4f2de9f1c4d2c732718281239c8ffb53c69c1d1d
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-04-07 04:37:30 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class Produto {
     protected int cod_Produto;
     protected String marca;
     protected String modelo;
     protected String descricao;
     protected float preco;
+    private static ArrayList<Produto> produtos = new ArrayList<Produto>();
 
     public Produto(String marca, String modelo, String descricao, float preco, int cod_Produto) {
         this.marca = marca;
@@ -13,6 +16,35 @@ public class Produto {
         this.cod_Produto = cod_Produto;
     }
 
+    public static boolean verificarLista(){
+        if (produtos.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void adicionarEstoqueProduto(Produto produto){
+        produtos.add(produto);
+    }
+
+    public static boolean verificarCod_Produto(int Cod_Produto) {
+        for (Produto produto : produtos) {
+            if (produto.getCod_Produto() == Cod_Produto) {
+                return true; // O código de produto já está cadastrado
+            }
+        }
+        return false; // O código de produto não está cadastrado
+    }
+
+    @Override
+    public String toString() {
+        return "Marca: " + marca +
+                ", Modelo: " + modelo +
+                ", Descrição: " + descricao +
+                ", Preço: " + preco +
+                ", Código do Produto: " + cod_Produto;
+    }
 
     public int getCod_Produto(){
         return cod_Produto;
